23eec4ef33514074f3dc488dfa7734894cfe8efb
Wise Dev <wisedev2699@gmail.com>
2022-05-04 18:11:26 +0500

    first nuxt code course in github

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Tutorial/>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+,
+  head(){
+    return {
+      title: 'Hello web ! '
+    }
+  }
+}
+</script>
---
+++ pages/post/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Hello web!</h1>
+</template>
+
+<script>
+export default {
+  name: "post",
+  head() {
+    return {
+      title: "Hello world",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
